b834d01a976c2354cb7abab3dcfd28f4207e2469
Fahim Shahriar <fahim.cse.uap@gmail.com>
2021-02-02 21:28:29 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,24 @@
+
+const age=0; // age = 4; const name="Fahim"; const ="";
+
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+
+}
+
+// Falsy............>
+// 1.0
+// 2.""
+// 3. undefined
+// 4. null
+// 5. NaN
+// 6. false
+
+// Truthy............>
+// 1. '0'
+// 2. " "
+// 3. [], {}
+// 4. true
