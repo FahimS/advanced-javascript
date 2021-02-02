6bd9b2011af77ff985256836d6800f2e1a8bc3c4
Fahim Shahriar <fahim.cse.uap@gmail.com>
2021-02-02 22:03:07 +0600

    double-vs-triple-equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,12 @@
+const first = 2; //first =1;
+const second = 3; // second="2"; //second = true;
+if (first == second) { //(first === second)
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+
+}
+
+// double equal ---> just check value
+// triple equal ---> check value and type
