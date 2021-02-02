0de29d1ec64a81dcabd1e93fb024047caaa90ee5
Fahim Shahriar <fahim.cse.uap@gmail.com>
2021-02-02 21:44:59 +0600

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,34 @@
+
+//undefined..............................
+
+let pakhi;
+console.log(pakhi);
+
+
+function add(num1,num2){
+    console.log(num1+num2);
+}
+const result = add(12, 15);
+console.log(result);
+
+
+function add(num1,num2){
+    console.log(num1, num2);
+}
+const result = add(12);
+console.log(result);
+
+
+const premik = {name:"smart dude", phone:34567};
+console.log(premik.gf);
+
+
+let fun =undefined;
+console.log(fun);
+
+
+
+// null.............................
+
+variable=null;
+console.log(variable);
