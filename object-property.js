2904a903be61a8836ac1d0ad55ea447c16bc1dba
Fahim Shahriar <fahim.cse.uap@gmail.com>
2021-02-02 22:37:23 +0600

    object-property.js

---
+++ object-property.js
@@ -0,0 +1,20 @@
+
+
+const students = [
+    { id: 21, name: 'Omar Sunny' },
+    { id: 31, name: 'Manna' },
+    { id: 41, name: 'Moyuri' },
+    { id: 51, name: 'Dipjol' }
+];
+
+const names = students.map(s => s.name);
+const id = students.map(s => s.id);
+const bigger = students.filter(s => s.id > 40);
+const biggerOne = students.find(s => s.id > 40);
+
+console.log(names);
+console.log(id);
+console.log(bigger);
+console.log(biggerOne);
+
+
