b0a040b24e63e241dbb0240185d95189a546cb14
Fahim Shahriar <fahim.cse.uap@gmail.com>
2021-02-02 22:53:56 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,11 @@
+
+bonus=20;
+
+function sum(first, second){
+    let result=first+second+bonus;
+    return result;
+    console.log(result);
+}
+const output=sum(3,5);
+// console.log(result);
+console.log(output);
